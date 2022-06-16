71298acb2b871f4134b162342b6d303a18a8954d
malikkanika9 <97448286+malikkanika9@users.noreply.github.com>
2022-06-16 10:25:43 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import { Chakra1 } from './components/Chakra1';
+function App() {
+ 
+  return (
+<div>
+  <Chakra1/>
+  <Projecttwo/>
+</div>
+  );
+}
+
+export default App;
---
+++ src/components/Chakra1.jsx
@@ -0,0 +1,41 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import { Flex, Text } from '@chakra-ui/react'
+import { Grid, GridItem } from '@chakra-ui/react'
+export const Chakra1=()=>{
+
+    return(
+
+        <div>
+                <ChakraProvider>
+    <Flex h="50vh" direction={{
+      base:"column",
+      sm:"column",
+      md:"column",
+      lg: "row",
+    }} color='white'>
+    <Flex flex=".2" justify="center" align="center" bg='skyblue'>
+    <Text>NAV</Text>
+  </Flex>
+  <Flex flex=".6" justify="center" align="center" bg='green'>
+    <Text>WIDGET</Text>
+  </Flex>
+  <Flex flex=".2" justify="center" align="center"  bg='red'>
+    <Text>CONTENT</Text>
+  </Flex>
+</Flex>
+<Grid h="50vh" templateColumns={{
+  base: 'repeat(1, 1fr)',
+  sm: 'repeat(1, 1fr)',
+  md: 'repeat(3, 1fr)',
+  lg:'repeat(3, 1fr)'
+}} gap={6}>
+  <GridItem w='100%' rowSpan={2} colSpan={2} h='44vh' bg='#0C2340' />
+  <GridItem w='100%' rowSpan={3} h='70vh' bg='orange' />
+  {/* <GridItem w='100%' h='20vh' bg='blue.500' /> */}
+  <GridItem w='100%' colSpan={2} h='20vh' bg='blue.500' />
+  {/* <GridItem w='100%' h='20vh' bg='blue.500' /> */}
+</Grid>
+    </ChakraProvider>
+        </div>
+    )
+}
